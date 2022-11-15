7373415e793e4c84ef531967ffa5c86b0f094ed1
yookyung Choi <ykc131719@gmail.com>
2022-11-15 16:21:10 +0900

    [FEAT] WebSecurity, HttpSecurity 설정

---
+++ mara/src/main/java/com/mara/mara/setting/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.mara.mara.setting;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //HttpSecurity
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().disable()
+                .csrf().disable()
+                .formLogin().disable()
+                .headers().frameOptions().disable();
+    }
+
+    //WebSecurity
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/image/**");
+    }
+}
