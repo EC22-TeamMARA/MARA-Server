ace1066a899a81c5a1ac1571ce3bc6dd063fceea
yookyung Choi <ykc131719@gmail.com>
2022-11-15 16:34:17 +0900

    [FEAT] WebMvcConfiguration 설정

---
+++ mara/src/main/java/com/mara/mara/setting/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.mara.mara.setting;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/image/**").addResourceLocations("classpath:/static/image/");
+    }
+}
