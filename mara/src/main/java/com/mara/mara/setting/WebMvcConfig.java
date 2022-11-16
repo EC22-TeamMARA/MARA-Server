c0017101aa75990a476cf4ae079178d14fe88015
yookyung Choi <ykc131719@gmail.com>
2022-11-16 22:42:56 +0900

    [FEAT] spring boot CORS 설정

--- mara/src/main/java/com/mara/mara/setting/WebMvcConfig.java
+++ mara/src/main/java/com/mara/mara/setting/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.mara.mara.setting;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,5 +12,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/image/**").addResourceLocations("classpath:/static/image/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://165.246.240.209:3000")
+                .allowedMethods("GET","POST","DELETE","PUT")
+                .allowCredentials(true);
     }
 }
